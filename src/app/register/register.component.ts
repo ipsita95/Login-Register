66b95a4a57adcec9fb4d11792f36e3ffedd57bfa
ipsita95 <munnimallick14@gmail.com>
2019-03-14 00:46:57 +0530

    Registration form added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class RegisterComponent {
+  form = new FormGroup({
+    name: new FormControl("", Validators.required),
+    title: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.email, Validators.required]),
+    password: new FormControl("", Validators.required)
+  });
+  get name() {
+    return this.form.get("name");
   }
-
+  get title() {
+    return this.form.get("title");
+  }
+  get email() {
+    return this.form.get("email");
+  }
+  get password() {
+    return this.form.get("password");
+  }
 }
