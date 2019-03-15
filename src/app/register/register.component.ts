921dbfa9001dbefded89446192a6b473ff23ee94
ipsita95 <munnimallick14@gmail.com>
2019-03-15 10:11:54 +0530

    validation added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,6 +5,7 @@ import {
   Validators,
   ReactiveFormsModule
 } from "@angular/forms";
+import { $ } from "protractor";
 
 @Component({
   selector: "app-register",
@@ -12,10 +13,20 @@ import {
   styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent {
+  ppattern = "[0-9]{10}";
+  postpattern = "[0-9]{6}";
   form = new FormGroup({
     name: new FormControl("", [Validators.required, Validators.minLength(3)]),
     title: new FormControl("", [Validators.required, Validators.minLength(3)]),
     email: new FormControl("", [Validators.email, Validators.required]),
+    phone: new FormControl("", [
+      Validators.required,
+      Validators.pattern(this.ppattern)
+    ]),
+    postcode: new FormControl("", [
+      Validators.required,
+      Validators.pattern(this.postpattern)
+    ]),
     password: new FormControl("", Validators.required),
     cpassword: new FormControl("", Validators.required)
   });
@@ -27,6 +38,12 @@ export class RegisterComponent {
   }
   get email() {
     return this.form.get("email");
+  }
+  get phone() {
+    return this.form.get("phone");
+  }
+  get postcode() {
+    return this.form.get("postcode");
   }
   get password() {
     return this.form.get("password");
